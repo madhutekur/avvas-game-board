1144fcae1b82e979fc9837810418f3a9a4ca402a
madhutekur <106869044+madhutekur@users.noreply.github.com>
2025-11-21 13:02:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -9,7 +10,9 @@ const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/avvas-game-board">
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
